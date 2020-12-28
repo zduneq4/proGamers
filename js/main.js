5ad6ea5671fc90d18d52e355218b9deb715e6a40
Mikołaj Zdunek <mrmikoz007@gmail.com>
2020-12-28 20:15:09 +0100

    fix navbar collapse

--- js/main.js
+++ js/main.js
@@ -10,4 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     window.addEventListener('scroll', addShadow)
+
+    $(document).ready(function () {
+        $(document).click(function (event) {
+            var clickover = $(event.target);
+            var _opened = $(".navbar-collapse").hasClass("show");
+            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+                $(".navbar-toggler").click();
+            }
+        });
+    });
 });
